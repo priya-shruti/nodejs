337b89b7fa42d5f1cd3491bff672b4227742278e
Shruti Priya <priya.shruti111@gmail.com>
2024-07-02 16:07:26 +0530

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
-
-const input = process.argv;
-
-if (input[2] == "add") {
-  fs.writeFileSync(input[3], input[4]);
-} else if (input[2] == "remove") {
-  fs.unlinkSync(input[3]);
-} else {
-  console.log("invalid input");
-}
+const path = require("path");
+const dirPath = path.join(__dirname, "files");
+// for (i = 0; i < 5; i++) {
+//   fs.writeFileSync(dirPath + "/hello" + i + ".txt", "a simple text file");
+// }
+fs.readdir(dirPath, (err, files) => {
+  files.forEach((item) => {
+    console.log(item);
+  });
+});
