123ab12eba3e889d4b518418a2695836d541f137
Shruti Priya <priya.shruti111@gmail.com>
2024-07-08 20:36:35 +0530

    commit

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const app = express();
 
-app.get("", (req, res) => {
-  res.send("<h1>Hello, this is home page</h1>");
+app.get("/", (req, resp) => {
+  resp.send("Welcome to home page");
 });
 
-app.get("/about", (req, res) => {
-  res.send(`<input type="text" placeholder="User name" />
-    <button>Click Me </button>`);
+app.get("/users", (req, resp) => {
+  resp.send("Welcome to users page");
 });
 
-app.get("/help", (req, res) => {
-  res.send({
-    name: "anil",
-    email: "aniltest@gmail.com",
-  });
-});
 app.listen(5000);
