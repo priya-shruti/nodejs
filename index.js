26dde1282f121b7aa0e7f6a47a56e18b77046148
Shruti Priya <priya.shruti111@gmail.com>
2024-08-15 12:26:48 +0530

    commit

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 const mongoose = require("mongoose");
-const main = async () => {
-  await mongoose.connect("mongodb://localhost:27017/e-comm");
-  const ProductSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
+mongoose.connect("mongodb://localhost:27017/e-comm");
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  brand: String,
+  category: String,
+});
+const saveInDB = async () => {
+  const Product = mongoose.model("products", ProductSchema);
+  let data = new Product({
+    name: "Note Pro",
+    price: 50,
+    brand: "abc",
+    category: "Mobile",
   });
-  const ProductModel = mongoose.model("products", ProductSchema);
-  let data = new ProductModel({ name: "M 10", price: 1000 });
   let result = await data.save();
   console.log(result);
 };
-main();
+
+const updateInDB = async () => {
+  const Product = mongoose.model("products", ProductSchema);
+  let data = await Product.updateOne(
+    { name: "M 10" },
+    {
+      $set: { price: 7000 },
+    }
+  );
+  console.log(data);
+};
+updateInDB();
